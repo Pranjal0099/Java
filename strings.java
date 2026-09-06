93f5a45b00523a45db5c4c9caebf3015c2c508b4
Pranjal0099 <pranjals7798@gmail.com>
2026-09-06 15:08:38 +0530

    update strings.java

---
+++ strings.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class strings {
+    public static void main(String  args[]){
+        String  name= "well come";
+        System.out.println(name);
+        System.out.println(name.length());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.charAt(5));
+        System.out.println(name.indexOf('e'));
+        System.out.println(name.trim());
+        System.out.println(name.replace("o","a"));
+    }
+}
